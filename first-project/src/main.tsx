303e5d17c672bf6658cf03837c9fb4b555f9ee7e
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-01 21:37:55 +0700

    add: React Router system

--- first-project/src/main.tsx
+++ first-project/src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import MyButton from './MyButton.tsx'
-import Heading from './Heading.tsx'
-import Section from './components/Section.tsx'
+import WrappedRoute from './components/WrappedRoute.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Heading title="Hello World!"/>
-    <App />
-    
-    <Section title="Section test"><MyButton/></Section>
+    <WrappedRoute />
   </React.StrictMode>,
 )
