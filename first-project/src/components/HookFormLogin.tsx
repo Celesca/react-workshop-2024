615b39fdefd894135af18d164efea152c58d537b
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-01 21:24:24 +0700

    fix: apply validation

--- first-project/src/components/HookFormLogin.tsx
+++ first-project/src/components/HookFormLogin.tsx
@@ -17,8 +17,8 @@ const HookFormLogin = () => {
     <>
         <h1>React Hook Form Login</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email"), { required: true, maxLength: 20}} />
-            <input {...register("password"), { required: true, maxLength: 50}} />
+            <input {...register("email", { required: true, maxLength: 20}) } />
+            <input {...register("password", { required: true, maxLength: 50})} />
             <input type="submit"/>
         </form>
     </>
